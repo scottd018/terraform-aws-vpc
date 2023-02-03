d97bbcb94573a6b1daedee4acd0cdb8395a1a364
Dustin Scott <dustin.scott18@gmail.com>
2023-02-03 16:42:03 -0600

    chore: remove unused comments
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- gateway.tf
+++ gateway.tf
@@ -9,33 +9,8 @@ resource "aws_internet_gateway" "internet_gateway" {
   tags = local.internet_gateway_tags
 }
 
-/* locals {
-  internet_subnets = var.vpc_connectivity == "public" ? {
-    for subnet in aws_subnet.public : subnet.id => subnet
-  } : {}
-} */
-
-/* resource "aws_network_interface" "nat_gateway" {
-  for_each = local.internet_subnets
-
-  subnet_id = each.value.id
-
-  tags = merge(
-    local.nat_gateway_tags,
-    { "Name" = "${var.vpc_name}-natgw-${each.value.availability_zone}" }
-  )
-} */
-
-/* locals {
-  internet_interfaces = var.vpc_connectivity == "public" ? {
-    for interface in aws_network_interface.nat_gateway : interface.id => interface
-  } : {}
-} */
-
 resource "aws_eip" "nat_gateway" {
   count = var.vpc_connectivity == "public" ? var.subnet_count : 0
-
-  # network_interface = each.value.id
 
   tags = merge(
     local.nat_gateway_tags,
